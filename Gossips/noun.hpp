b3506a2936716de3b17820320075951da29796a9
ErrantConstruct <errantconstruct@gmail.com>
2016-07-16 19:24:51 -0400

    Moving noun to own file

---
+++ Gossips/noun.hpp
@@ -0,0 +1,37 @@
+//
+//  noun.hpp
+//  Gossips
+//
+//  Created by Matt Postema on 7/16/16.
+//  Copyright © 2016 Matt Postema. All rights reserved.
+//
+
+#ifndef noun_hpp
+#define noun_hpp
+
+#include <iostream>
+
+enum noun
+{
+    dog,
+    cat,
+    mayor,
+    priest,
+    woman
+};
+
+std::ostream& operator<<(std::ostream& os, noun c)
+{
+    switch(c)
+    {
+        case dog   : os << "dog";    break;
+        case cat   : os << "cat"; break;
+        case mayor : os << "mayor";  break;
+        case priest: os << "priest";   break;
+        case woman: os << "woman"; break;
+        default    : os.setstate(std::ios_base::failbit);
+    }
+    return os;
+}
+
+#endif /* noun_hpp */
