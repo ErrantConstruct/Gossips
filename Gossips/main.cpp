4c760ede873b0608de319cdce4d9d7c1e0868179
ErrantConstruct <errantconstruct@gmail.com>
2016-07-16 18:27:55 -0400

    Initial Commit

---
+++ Gossips/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Gossips
+//
+//  Created by Matt Postema on 7/16/16.
+//  Copyright © 2016 Matt Postema. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
